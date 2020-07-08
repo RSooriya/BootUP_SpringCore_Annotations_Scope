ea9e0aae0791a62fa9c5702898b1c8b505ca715d
RSooriya <soormo06@gmail.com>
2020-07-08 18:15:51 +0530

    Comments added

--- src/com/sooriya/BaseballCoach.java
+++ src/com/sooriya/BaseballCoach.java
@@ -3,6 +3,7 @@ package com.sooriya;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//@Scope - used to specify bean scope
 @Component
 @Scope("prototype")
 public class BaseballCoach implements Coach {
--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -6,8 +6,10 @@ public class MyApp {
 
 	public static void main(String args[])
 	{
+		//creating spring container and loading configuration file into the container
 		ClassPathXmlApplicationContext context = new
 				ClassPathXmlApplicationContext("applicationContext.xml");
+		//retrieving beans from container
 		Coach theCoach = context.getBean("baseballCoach",Coach.class);
 		Coach alphaCoach = context.getBean("baseballCoach",Coach.class);
 		System.out.println("Singleton "+ (theCoach==alphaCoach));
